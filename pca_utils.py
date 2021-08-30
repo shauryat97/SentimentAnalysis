a10a5fc1d31be5a3d29383030d96604cd04cb2c6
shauryat97 <shaurya1997.18@gmail.com>
2021-08-31 03:02:47 +0530

    recommit after git add pca_utils.py

--- pca_utils.py
+++ pca_utils.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-
+from sklearn.decomposition import PCA 
 
 def get_XafterMean(X,N):  
     mean = np.sum(X,axis= 0)/N
@@ -11,35 +10,17 @@ def get_XafterMean(X,N):
             XafterMean[i,j]=X[i,j] - mean[j]
     return XafterMean,mean
 
-def get_XXtranspose(XafterMean):
-    return XafterMean@np.transpose(XafterMean)
+def get_XafterMean_test(X,mean):
+    XafterMean = X.copy()
+    for j in range(X.shape[1]):
+        for i in range(X.shape[0]):
+            XafterMean[i,j]=X[i,j] - mean[j]
+    return XafterMean
 
-
-
-def eigen(XX_t):
-    return  np.linalg.eig(XX_t)
-
-def eigsorted(eigenval,eigenvec):
-    indexInDescendingOrder = eigenval.argsort()[::-1]   
-    eigenval = eigenval[indexInDescendingOrder]
-    eigenvec = eigenvec[:,indexInDescendingOrder]
-    return eigenval,eigenvec
-
-
-def get_U(N,XafterMean,eigenvec):
-    U =np.zeros((10201,N))
-    for i in range(N):
-        U[:,i]=(XafterMean.T)@eigenvec[:,i]
-        U[:,i]/= np.linalg.norm(U[:,i],ord = 2)
-    return U
-
-#Compute PCA
-def toPCA(N,XafterMean,Uprime,k):
-    Y = []
-    for i in range(N):
-        yi = Uprime.T.dot(XafterMean[i,:])
-        yi.tolist()
-        Y.append(yi)
-    Y = np.array(Y)
-    Y = np.reshape(Y,(N,k))
-    return Y
+def calc_pca(train,test,k):
+    
+    pca = PCA(n_components = k)
+    pca.fit(train)
+    train = pca.transform(train)
+    test = pca.transform(test)
+    return train,test
