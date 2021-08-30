9e9ff13885a66b8d7556ba16624774fb6620c836
shauryat97 <shaurya1997.18@gmail.com>
2021-08-30 10:33:29 +0530

    added preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,36 @@
+import re
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+def preprocess(review_lst):
+    docs = []
+    labels = []
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    for line in review_lst:
+       sent = line.lower()
+    #    sent = line.strip().lower() # converting all characters to lowercase.
+       labels.append(int(sent[-2])) # extracting labels 1 or 0 
+       sent = sent[:len(sent)-2]
+       alnum_string = re.sub(r"[^a-zA-Z0-9]"," ",sent) # removing any special characters.
+       filtered_string = ""
+       for word in alnum_string.split():
+           if word not in stop_words:    # removing stopwords
+               filtered_string+=word+" "
+       lemmatised_string = ""
+       for word in filtered_string:
+           lemmatised_string+= lemmatizer.lemmatize(word)   # lemmatizing
+       string_lst = word_tokenize(lemmatised_string) # tokenize
+       docs.append(string_lst)
+    return docs,labels
+ 
+def listToString(s): 
+
+    string = " " 
+    return (string.join(s))
