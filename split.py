91b2fc7f56732a1e1b2b0a2e40b8eceaadfb575e
shauryat97 <shaurya1997.18@gmail.com>
2021-08-30 10:32:52 +0530

    added split.py

---
+++ split.py
@@ -0,0 +1,14 @@
+import random
+
+
+def split_set(file):
+    lines_lst = file.readlines() # will return a list of reviews.
+    train_set = []
+    test_set = []
+    l = [i for i in range(1000)]
+    random.shuffle(l)
+    for i in l[:750]:
+        train_set.append(lines_lst[i])
+    for i in l[750:]:
+        test_set.append(lines_lst[i])
+    return train_set,test_set
